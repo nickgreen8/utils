418c2e57c7fc1b871b09b12ef74d334772845185
Nick Green <nick@nickgreenweb.co.uk>
2015-01-17 23:53:30 +0000

    First attempted setup

---
+++ src/Config.php
@@ -0,0 +1,94 @@
+<?php
+namespace N8G\Utils;
+
+use N8G\Utils\Log;
+
+/**
+ * This class acts as a central storage for all config data. This can be used in many
+ * situations and on different sites. This is a static class and can be called from
+ * anywhere.
+ *
+ * @author Nick Green <nick-green@live.co.uk>
+ */
+class Config
+{
+	/**
+	 * Array of data to be used as the data store
+	 * @var array
+	 */
+	private static $data = array();
+
+	/**
+	 * This function is used to initilised the config object. The data is retreved
+	 * from the database and stored within the data array. Nothing is passed and nothing
+	 * is retuned.
+	 *
+	 * @return void
+	 */
+	public static function init()
+	{
+		Log::info('Initilising config class');
+	}
+
+	/**
+	 * This function is used to set a new item of data in the config data store. The
+	 * function takes the name and the value of the data. Nothing is returned.
+	 *
+	 * @param  string $name  The name and the key to the data being stored
+	 * @param  mixed  $value The value of the data
+	 * @return void
+	 */
+	public static function setItem($name, $value)
+	{
+		Log::info(sprintf('Setting Config[\'%s\'] to "%s"', $name, $value));
+		//Set data in config
+		self::$data[$name] = $value;
+	}
+
+	/**
+	 * This function is used to get an item of data from the config data store. The
+	 * function takes the name of the data and returns it's value.
+	 *
+	 * @param  string $name  The name and the key to the data being stored
+	 * @return mixed         The value attributed to the key passed
+	 */
+	public static function getItem($name)
+	{
+		Log::info(sprintf('Getting Config[\'%s\']', $name));
+		//Check data is stored
+		if (isset($name)) {
+			//Return the data
+			return self::$data[$name];
+		}
+		return NULL;
+	}
+
+	/**
+	 * This function is used to check if an item is set in the config storatge bank.
+	 * The name of the item to be requested is passed to the function and if it is set,
+	 * the data is returned. If the data is not present, false is returned.
+	 *
+	 * @param  string $name The name of the item to check.
+	 * @return mixed        The item of data if it is set. If it is not, FALSE.
+	 */
+	public static function inConfig($name)
+	{
+		Log::info(sprintf('Looking for %s in config', $name));
+		//Check for element in config
+		if (isset(self::$data[$name])) {
+			return self::$data[$name];
+		}
+		//return default
+		return false;
+	}
+
+	/**
+	 * This function is used to reset the config data store.
+	 *
+	 * @return void
+	 */
+	public static function clear()
+	{
+		self::$data = array();
+	}	
+}
---
+++ src/Log.php
@@ -0,0 +1,250 @@
+<?php
+namespace N8G\Utils;
+
+/**
+ * This function acts as a logging mechinism. This can be used in many situations
+ * and on different sites. This is a static class and can be called from anywhere.
+ *
+ * @author Nick Green <nick-green@live.co.uk>
+ */
+class Log
+{
+	//Log categoies
+	const FATAL   = 0;
+	const ERROR   = 1;
+	const WARN    = 2;
+	const NOTICE  = 3;
+	const INFO    = 4;
+	const DEBUG   = 5;
+	const SUCCESS = 6;
+
+	/**
+	 * The spacing needed to format the log file
+	 */
+	const LOG_SPACING = PHP_EOL;
+
+	/**
+	 * Instance of this class
+	 * @var object
+	 */
+	private static $instance;
+
+	/**
+	 * The file to be written to
+	 * @var pointer
+	 */
+	private static $file;
+
+	/**
+	 * This is the default constructor. The directory to put the files as well as the
+	 * name of the file.
+	 *
+	 * @param string $directory The folder to put the log files
+	 * @param string $filename  The name of the file to create
+	 */
+	private function __construct($directory, $filename)
+	{
+		//set the name of the file
+		self::$file = $this->getFile($directory, $filename);
+	}
+
+	/**
+	 * Closes the file if needed.
+	 */
+	public function __destruct()
+	{
+		if (self::$file !== null) {
+			fclose(self::$file);
+		}
+	}
+
+	/**
+	 * This function is used to initilise the log file. The directory and the file
+	 * name is passed as well as the name of the file to be set.
+	 *
+	 * @param  string $directory The folder to hold the log file
+	 * @param  string $filename  The name of the log file
+	 * @return void
+	 */
+	public static function init($directory = 'logs/', $filename = NULL)
+	{
+		$directory = $directory;
+		//Check for instance of the class
+		if (self::$instance === null) {
+			self::$instance = new self($directory, $filename);
+		}
+	}
+
+	/**
+	 * This function is used to setup or create the holding folder and the log file.
+	 * The directory of the file is passed and if it does not exist, it is created.
+	 * The name of the file to be created is also passed.
+	 *
+	 * @param  string $directory The file where the file is
+	 * @param  string $filename  The name of the file
+	 * @return void
+	 */
+	private static function getFile($directory, $filename)
+	{
+		//Create file name if there is none
+		if ($filename === NULL) {
+			$filename = date('Y-m-d') . '.log';
+		}
+
+		//Check the director exists
+		if (!is_dir($directory)) {
+			mkdir($directory, 0777, true);
+		}
+
+		try {
+			//Check the files exists
+			if (false === self::$file = fopen($directory . $filename, 'a')) {
+				throw new UnableToOpenFileException('Could not create log file!');
+			}
+			return self::$file;
+		} catch (UnableToOpenFileException $e) {}
+	}
+
+	/**
+	 * This function is used to write the the relevant log file. The category of the
+	 * message and the message is passed to the function. Nothign is returned.
+	 *
+	 * @param  int    $cat The category of the message
+	 * @param  string $msg The message to be written to the file
+	 * @return void
+	 */
+	private static function writeToFile($cat, $msg)
+	{
+		//check if the class has been initiated
+		self::init();
+
+		$message = sprintf(
+			'{%s} %s - %s%s',
+			date('d\/m\/Y H:i:s'),
+			self::getCategory($cat),
+			$msg,
+			PHP_EOL
+		);
+		fwrite(self::$file, $message);
+	}
+
+	/**
+	 * This function is used to specify the category in a log message. The severity
+	 * is passed to the function and the string representing it is returned.
+	 *
+	 * @param  int    $cat The constant for the log message category
+	 * @return string      The string for the category part of the the log message
+	 */
+	private static function getCategory($cat)
+	{
+		switch ($cat) {
+			case self::FATAL :
+				// $colour = '0;31';
+				$category = 'FATAL  ';
+				break;
+			case self::ERROR :
+				// $colour = '1;33';
+				$category = 'ERROR  ';
+				break;
+			case self::WARN :
+				// $colour = '0;35';
+				$category = 'WARNING';
+				break;
+			case self::NOTICE :
+				// $colour = '0;36';
+				$category = 'NOTICE ';
+				break;
+			case self::INFO :
+				// $colour = '0;34';
+				$category = 'INFO   ';
+				break;
+			case self::DEBUG :
+				// $colour = '0;32';
+				$category = 'DEBUG  ';
+				break;
+			case self::SUCCESS :
+				// $colour = '0;32';
+				$category = 'SUCCESS';
+				break;
+		}
+		// return sprintf("\033[%sm %s \033[0m ", $colour, $category);
+		return $category;
+	}
+
+	/**
+	 * This method is used to log a fatal error.
+	 *
+	 * @param  string $msg The message to be logged
+	 * @return void
+	 */
+	public static function fatal($msg)
+	{
+		self::writeToFile(self::FATAL, $msg);
+	}
+
+	/**
+	 * This method is used to log an error.
+	 *
+	 * @param  string $msg The message to be logged
+	 * @return void
+	 */
+	public static function error($msg)
+	{
+		self::writeToFile(self::ERROR, $msg);
+	}
+
+	/**
+	 * This method is used to log a warning.
+	 *
+	 * @param  string $msg The message to be logged
+	 * @return void
+	 */
+	public static function warn($msg)
+	{
+		self::writeToFile(self::WARN, $msg);
+	}
+
+	/**
+	 * This method is used to log a notice.
+	 *
+	 * @param  string $msg The message to be logged
+	 * @return void
+	 */
+	public static function notice($msg)
+	{
+		self::writeToFile(self::NOTICE, $msg);
+	}
+
+	/**
+	 * This method is used to log an info message.
+	 *
+	 * @param  string $msg The message to be logged
+	 * @return void
+	 */
+	public static function info($msg)
+	{
+		self::writeToFile(self::INFO, $msg);
+	}
+
+	/**
+	 * This method is used to log a debug message.
+	 *
+	 * @param  string $msg The message to be logged
+	 * @return void
+	 */
+	public static function debug($msg)
+	{
+		self::writeToFile(self::DEBUG, $msg);
+	}
+
+	/**
+	 * This method is used to log a success.
+	 *
+	 * @param  string $msg The message to be logged
+	 * @return void
+	 */
+	public static function success($msg)
+	{
+		self::writeToFile(self::SUCCESS, $msg);
+	}
+}
